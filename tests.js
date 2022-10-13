b1693aa37239e33562953db4bcb3d36bd5a8ae6d
Javier Razo <javier.razo1221@gmail.com>
2022-10-13 11:43:39 -0700

    first exercise testing

--- tests.js
+++ tests.js
@@ -13,3 +13,9 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+//Test 1
+describe("sayHello", function () {
+it("should be a defined function", function () {
+    expect(typeof sayHello).toBe("function");
+});
+});
